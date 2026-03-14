09ca2f98392656fec8200329170b5d4a2fb08b46
Mate Abhishek <abhishekworkspace31@gmail.com>
2026-03-14 22:44:32 +0530

    Update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '/api'
+  baseURL: 'https://psychelens-fullstack.onrender.com/api'
 });
 
 api.interceptors.request.use((config) => {
